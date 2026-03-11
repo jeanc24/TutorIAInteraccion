d6a078718bb167dcf166d9215cdd4645f8d490e3
Jean Carlos Perez <jcpo0002@ce.pucmm.edu.do>
2026-03-11 15:08:59 -0400

    Initial Commit

---
+++ src/main/java/icc354/pucmm/tutoriainteraccion/TutorIaInteraccionApplication.java
@@ -0,0 +1,13 @@
+package icc354.pucmm.tutoriainteraccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorIaInteraccionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorIaInteraccionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icc354/pucmm/tutoriainteraccion/TutorIaInteraccionApplicationTests.java
@@ -0,0 +1,13 @@
+package icc354.pucmm.tutoriainteraccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorIaInteraccionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
